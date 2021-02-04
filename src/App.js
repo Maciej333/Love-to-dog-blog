19f96c197cc94c287ae0d75aca699dcea72936f3
Maciej Rogowski <maciej.rogowski333@gmail.com>
2021-02-04 15:44:13 +0100

    add Page not found and fix spinner in gallery

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Gallery } from './pages/Gallery';
 import { Home } from './pages/Home';
 import { Posts } from './pages/Posts';
+import { NotFound } from './pages/NotFound';
 
 function App() {
 
@@ -19,7 +20,7 @@ function App() {
             <Gallery />
           </Route>
           <Route path="*">
-
+            <NotFound />
           </Route>
         </Switch>
       </div>
